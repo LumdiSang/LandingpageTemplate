bac93beeb05730047c10a5367f81527ad2a8badb
Lumdiba Sangtam <lumdiba.sangtam@gmail.com>
2022-08-08 16:11:35 +0530

    Committed

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = document.getElementById('search');
+const searchBar = document.getElementById('searchBar');
+
+searchBar.addEventListener('click', ()=>{
+    search.classList.toggle('show');
+})
